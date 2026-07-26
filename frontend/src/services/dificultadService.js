850746f65f378d759fa50321b956fc532a7d39f1
Dante Luca Ortega <dlortega@fi.uba.ar>
2026-07-26 18:55:06 -0300

    Update dificultadService.js

--- frontend/src/services/dificultadService.js
+++ frontend/src/services/dificultadService.js
@@ -2,12 +2,12 @@ import { api } from './api';
 
 // GET: Obtener todas las dificultades
 export const getDificultades = async () => {
-    const response = await api.get('/dificultades');
+    const response = await api.get('/api/dificultades');
     return response.data;
 };
 
 // GET: Obtener todos los niveles de una dificultad
 export const getNivelesPorDificultad = async (dificultadId) => {
-    const response = await api.get(`/niveles?dificultad=${dificultadId}`);
+    const response = await api.get(`/api/niveles?dificultad=${dificultadId}`);
     return response.data;
 };
