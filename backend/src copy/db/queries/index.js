be4facd1479a6ab22f1ffcc7a7a0f625d3b216cd
Dante Luca Ortega <dlortega@fi.uba.ar>
2026-07-26 22:30:19 -0300

    Use un backend desactualizado

--- backend/src copy/db/queries/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  dificultad: require('./dificultad.queries'),
-  levels: require('./levels.queries'),
-  scores: require('./scores.queries'),
-  pistas: require('./pistas.queries'),
-  powerups: require('./powerups.queries'),
-}
